10cadf55ba3e6680fdbbbc3a82e9c2de2b9535c8
younes_qasempour <qasempouryounes@gmail.com>
2025-03-05 23:54:34 +0330

    Initial commit

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from dotenv import load_dotenv
 
+# TODO 1: https://appbrewery.github.io/Zillow-Clone/
 
 
+# TODO 2: https://docs.google.com/forms/u/0/
+# TODO 3: file:///D:/100%20Days%20of%20Code/53%20-%20Day%2053%20-%20Intermediate+%20Web%20Scraping%20Capstone%20-%20Data%20Entry%20Job%20Automation/002%20Web%20Scraping%20and%20Data%20Entry%20Capstone%20Project%20Requirements.html
+# TODO 4: file:///D:/100%20Days%20of%20Code/53%20-%20Day%2053%20-%20Intermediate+%20Web%20Scraping%20Capstone%20-%20Data%20Entry%20Job%20Automation/003%20Hints%20&%20Solution.html
 
 
 
